dd922e072fd309970dc5978eb1592000d1c9291d
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-27 00:02:46 +0530

    added - userModel

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,18 +9,6 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-//   res.send("hello user, you are logged in")
-// })
-
-// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-//     res.send("hello user, you are logged in and you can delete your account")
-// });
-
-// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
-//   res.send("hello admin, you are logged in and you can delete all accounts")
-// })
-
 //UPDATE
 router.put("/:id", verifyUser, updateUser);
 
@@ -30,7 +18,7 @@ router.delete("/:id", verifyUser, deleteUser);
 //GET
 router.get("/:id", verifyUser, getUser);
 
-//GET ALL
+//GET ALL 
 router.get("/", verifyAdmin, getUsers);
 
 export default router;
