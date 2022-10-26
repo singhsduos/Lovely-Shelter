11c65d79983bd6dae8f1c5985eb7ae337f0e4ae4
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-26 23:58:12 +0530

    added - loginMethod

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,13 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
     try {
+        //generating hashing method here
         const salt = bcrypt.genSaltSync(10);
+        //and now we use this function to encrypt our password
         const hashedPass = bcrypt.hashSync(req.body.password, salt);
 
         const newUser = new User({
@@ -21,7 +24,29 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
     try {
-           
+      //search the user by username in db
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) return next(createError(404, "User not found!"));
+
+      //compare the password from db and by user
+      const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!isPasswordCorrect)
+        return next(createError(400, "Wrong password or username!"));
+
+      //generate the token for login
+    //basically token will store all the info of user and check the weather the user is admin or not
+       const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin },process.env.JWT);
+
+      const { password, isAdmin, ...otherDetails } = user._doc; //doc is because we described a auth model inside user model
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json({ details: { ...otherDetails }, isAdmin });
     } catch (err) {
         next(err);
        }
