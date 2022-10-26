10b5a97df8ee08c381c95fe3985286d5ea409d7a
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-26 20:23:05 +0530

    added - CRUD for hotels

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,10 +1,27 @@
 import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
 
+//CREATE
+router.post("/", createHotel);
+
 //UPDATE
+router.put("/:id", updateHotel);
+
 //DELETE
+router.delete("/:id", deleteHotel);
+
 //GET
-//GETALL
+router.get("/find/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
 
 export default router;
