97891c83c54b8b51c45029a97096fa9c4a800a42
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-28 16:20:35 +0530

    updated - homePage

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import Header from '../../components/header/Header';
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
+import MailList from "../../components/mailList/MailList";
+import PropertyList from "../../components/propertyList/PropertyList.jsx";
 import "./home.css";
 
 const Home = () => {
   return (
-      <div>
-          <Navbar />
-          <Header />
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
-  )
+    </div>
+  );
 }
 
 export default Home;
