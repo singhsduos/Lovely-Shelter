1ba84588536f084d72def47b70b9e7f30330b0d8
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-30 00:16:23 +0530

    Create FeaturedProperties.jsx

---
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,36 @@
+import "./featuredProperties.css";
+import useFetch from "../../hooks/useFetch";
+import React from 'react'
+
+const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/api/hotels?featured=true&limit=4");
+  console.log(data);
+  return (
+    <div className="fp">
+  {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img src={item.photos[0]} alt="" className="fpImg" />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">
+                Starting from ${item.cheapestPrice}
+              </span>
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}  
+    </div>
+  );
+}
+
+export default FeaturedProperties;
