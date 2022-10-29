b3771c82c6556bb9f1b0f3325bf5e8b17e578002
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-30 00:17:03 +0530

    added - apiForPropertType

--- frontend/src/components/propertyList/PropertyList.jsx
+++ frontend/src/components/propertyList/PropertyList.jsx
@@ -1,10 +1,8 @@
-import "./propertyList.css";
-
 import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  // const { data, loading, error } = useFetch("/hotels/countByType");
+  const { data, loading, error } = useFetch("/api/hotels/countByType");
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -16,23 +14,24 @@ const PropertyList = () => {
 
   return (
     <div className="pList">
-  
- 
+      {loading ? (
+        "loading"
+      ) : (
         <>
-          {
+          {data &&
             images.map((img, i) => (
               <div className="pListItem" key={i}>
                 <img src={img} alt="" className="pListImg" />
                 <div className="pListTitles">
-                  <h1>Type</h1>
+                  <h1>{data[i]?.type}</h1>
                   <h2>
-                    count
+                    {data[i]?.count} {data[i]?.type}
                   </h2>
                 </div>
               </div>
             ))}
         </>
-      
+      )}
     </div>
   );
 };
