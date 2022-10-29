8971805f1edadc60c30220c71a2365062f4eb261
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-30 00:17:30 +0530

    update -searchListWithAPI

--- frontend/src/components/searchItem/SearchItem.jsx
+++ frontend/src/components/searchItem/SearchItem.jsx
@@ -1,41 +1,51 @@
+import { Link } from "react-router-dom";
 import "./searchItem.css";
+import React from "react";
+import {
+faLocationDot
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import React from 'react'
-
-const SearchItem = () => {
+const SearchItem = ({ item }) => {
   return (
     <div className="searchItem">
-      <img
-        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-        alt=""
-        className="siImg"
-      />
+      <img src={item.photos[0]} alt={item.name} className="siImg" />
       <div className="siDesc">
-        <h1 className="siTitle">Tower</h1>
-        <span className="siDistance">5km from center</span>
+        <div>
+          <h1 className="siTitle">{item.name}</h1>
+          <div className="siLocation">
+            <FontAwesomeIcon icon={faLocationDot} className="siLocIcon" />
+            <span>{item.city}</span>
+          </div>
+        </div>
+        <span className="siDistance">{item.distance} Km from center</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
         </span>
-        <span className="siFeatures">Bootiful environvent</span>
+        <span className="siFeatures">{item.desc}</span>
         <span className="siCancelOp">Free cancellation </span>
         <span className="siCancelOpSubtitle">
           You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className="siDetails">
-        <div className="siRating">
-          <span>Excellent</span>
-          <button>8.9</button>
-        </div>
+        {item.rating && (
+          <div className="siRating">
+            <span>Excellent</span>
+            <button>{item.rating}</button>
+          </div>
+        )}
         <div className="siDetailTexts">
-          <span className="siPrice">123$</span>
+          <span className="siPrice">${item.cheapestPrice}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <button className="siCheckButton">See availability</button>
+          <Link to={`/hotels/${item._id}`}>
+            <button className="siCheckButton">See availability</button>
+          </Link>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default SearchItem
+export default SearchItem;
