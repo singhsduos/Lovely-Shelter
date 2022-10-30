9fd194d739aed4516a2bf1f5bfb4a6aaa2b0e64e
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-31 00:16:07 +0530

    update - navbarWithApi

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">Lovely Shelter</span>
         </Link>
-
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <h3 className="username">{user.username}</h3>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <Link to="/login" style={{ color: "inherit", textDecoration: "none" }}>
+              <button className="navButton">Login</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
