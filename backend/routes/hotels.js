96c5758dc37241f34e196f00d83d54f8f8c45416
singhsduos <neelesh.singh@jalantechnologies.com>
2022-10-31 00:15:16 +0530

    added - api for boking room

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -7,6 +7,7 @@ import {
   updateHotel,
   countByCity,
   countByType,
+  getHotelRooms,
 } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -29,5 +30,6 @@ router.get("/", getHotels);
 
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
